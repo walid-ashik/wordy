b2be436ce986d7bb3dcdde9d8ece50b9f23c530f
Walid.al.ashik <walid.al.ashik@gmail.com>
2019-11-22 19:43:02 +0600

    Added bottom navigation bar in gameplay page

--- lib/ui/gameplay.dart
+++ lib/ui/gameplay.dart
@@ -3,8 +3,8 @@ import 'package:Wordy/util/utils.dart';
 import 'package:flutter/material.dart';
 
 class GamePlayPage extends StatefulWidget {
-
   final Category category;
+
   GamePlayPage(this.category) : super();
 
   @override
@@ -12,19 +12,100 @@ class GamePlayPage extends StatefulWidget {
 }
 
 class _GamePlayPageState extends State<GamePlayPage> {
-
   Category category;
-  _GamePlayPageState(Category category){
+
+  _GamePlayPageState(Category category) {
     this.category = category;
   }
 
   @override
   Widget build(BuildContext context) {
+    int selectedBottomItem = 0;
+
     return Scaffold(
-      backgroundColor: HexColor('${category.color}'),
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomAppBar(
+        color: HexColor('${category.color}'),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.arrow_back, color: HexColor('#444444'),),
+                onPressed: () {
+                  debugPrint('clicked');
+                },
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.arrow_forward, color: HexColor('#444444')),
+                onPressed: () {
+                  debugPrint('clicked2');
+                },
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.refresh, color: HexColor('#444444')),
+                onPressed: () {
+                  debugPrint('clicked3');
+                },
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.check, color: HexColor('#444444')),
+                onPressed: () {
+                  debugPrint('clicked4');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Text('${category.name}'),
       ),
     );
   }
 }
+
+//BottomNavigationBar(
+//currentIndex: 0,
+//onTap: (int index) {
+////          setState(() {
+////            selectedBottomItem = index;
+////          });
+//},
+//items: [
+//BottomNavigationBarItem(
+//backgroundColor:HexColor('${category.color}'),
+//icon: Icon(
+//Icons.arrow_back,
+//color: HexColor('#444444'),
+//),
+//title: Text('')),
+//BottomNavigationBarItem(
+//backgroundColor:HexColor('${category.color}'),
+//icon: Icon(
+//Icons.arrow_forward,
+//color: HexColor('#444444'),
+//),
+//title: Text('')),
+//BottomNavigationBarItem(
+//backgroundColor:HexColor('${category.color}'),
+//icon: Icon(
+//Icons.refresh,
+//color: HexColor('#444444'),
+//),
+//title: Text('')),
+//BottomNavigationBarItem(
+//backgroundColor:HexColor('${category.color}'),
+//icon: Icon(
+//Icons.check,
+//color: HexColor('#444444'),
+//),
+//title: Text(''))
+//],
+//),
