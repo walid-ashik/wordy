a840b5e4bd6c616612b95b8e3bf9a9fafc506d86
Walid.al.ashik <walid.al.ashik@gmail.com>
2019-12-08 23:36:11 +0600

    finally can save user's last played word index from list

--- lib/ui/gameplay.dart
+++ lib/ui/gameplay.dart
@@ -26,7 +26,7 @@ class _GamePlayPageState extends State<GamePlayPage> {
   List<Word> wordList = [];
   String guessedWord = '';
   List<String> charList = [];
-  var index = 0;
+  int index = 0;
   Word preposition = new Word.empty();
   var isGeneratedNewList = true;
   String correctWord = '';
@@ -38,17 +38,11 @@ class _GamePlayPageState extends State<GamePlayPage> {
 
   SharedPreferences prefes;
 
+  bool isFirstLoading = true;
+
   _GamePlayPageState(Category category) {
     this.category = category;
     homeText = category.name;
-//    setCategoryList(category.name);
-//
-//    preposition = wordList[index];
-//    totalScroe = wordList.length;
-
-//    setList();
-//    testJson();
-//    fetchListAsString(category.name);
   }
 
   @override
@@ -65,8 +59,7 @@ class _GamePlayPageState extends State<GamePlayPage> {
         timeInSecForIos: 1,
         backgroundColor: Colors.red,
         textColor: Colors.white,
-        fontSize: 16.0
-    );
+        fontSize: 16.0);
   }
 
   Future loadJson() async {
@@ -77,7 +70,6 @@ class _GamePlayPageState extends State<GamePlayPage> {
 //    });
 //
     prefes = await SharedPreferences.getInstance();
-
 //    print(prefs.getString(category.name.toLowerCase()));
 
     WordyPreference.getString(category.name.toLowerCase()).then((string) {
@@ -88,9 +80,8 @@ class _GamePlayPageState extends State<GamePlayPage> {
 //
         var stringId = '${word['id']}';
         int id = int.parse(stringId);
-        wordList.add(
-            new Word.Construct(id, '${word['word']}', '${word['meaning']}',
-                '${word['fillInTheGapSentence']}'));
+        wordList.add(new Word.Construct(id, '${word['word']}',
+            '${word['meaning']}', '${word['fillInTheGapSentence']}'));
         debugPrint('${word['id']}');
         debugPrint('${word['word']}');
         debugPrint('${word['meaning']}');
@@ -101,25 +92,6 @@ class _GamePlayPageState extends State<GamePlayPage> {
         isLoaded = true;
       });
     });
-
-//    List<Word> words = await WordyPreference.fetchListAsString(category.name);
-//
-//    print('fetcing from preferences: length - ${words.length}');
-//
-//    for(int i=0; i<words.length; i++){
-//      print('\n');
-//      print(words[i].word);
-////      print(word.id.toString());
-//    }
-//    print('fetching done!');
-//
-//  }
-
-
-//  void setList() async {
-//    List<Word> l = await WordyPreference.fetchListAsString(category.name);
-//    list = l;
-//  }
 
     void testJson() {
       List<Word> l = DataUtil.getPrepositionList();
@@ -133,48 +105,9 @@ class _GamePlayPageState extends State<GamePlayPage> {
 
       debugPrint('$myData');
 
-//    String wordListInString = json.encode(l);
-//    List<Word> list = json.decode(myData);
-////
-//    for (var word in myData) {
-//      debugPrint('\n');
-//      debugPrint('${word['id']}');
-//      debugPrint('${word['word']}');
-//      debugPrint('${word['meaning']}');
-//      debugPrint('${word['fillInTheGapSentence']}');
-//    }
     }
   }
 
-//  fetchListAsString(String categoryName) async {
-//    final prefs = await SharedPreferences.getInstance();
-//
-//    if (prefs.containsKey('preposition')) {
-//      debugPrint('Contains key');
-//
-//      var data = prefs.get('preposition') as String;
-//      var myData = json.decode(data);
-//
-//      for (var word in myData) {
-//
-//        var stringId = '${word['id']}';
-//        int id = int.parse(stringId);
-//        wordList.add(new Word(id, '${word['word']}', '${word['meaning']}',
-//            '${word['fillInTheGapSentence']}'));
-////       debugPrint('${word['id']}');
-////      debugPrint('${word['word']}');
-////      debugPrint('${word['meaning']}');
-////      debugPrint('${word['fillInTheGapSentence']}');
-//      }
-//    } else {
-//      debugPrint('Not contains key');
-//    }
-//
-//
-//    for (var l in wordList) {
-//      debugPrint(l.word);
-//    }
-//  }
 
   void setCategoryList(String categoryName) {
     if (categoryName.toLowerCase() == Categories.preposition.toLowerCase()) {
@@ -189,173 +122,189 @@ class _GamePlayPageState extends State<GamePlayPage> {
 
   @override
   Widget build(BuildContext context) {
-    int selectedBottomItem = 0;
 
     if (isLoaded) {
+
+      if(isFirstLoading){
+        isFirstLoading = false;
+
+        var categoryName = category.name.toLowerCase();
+        var indexSavingKey = categoryName + 'index';
+
+        if (prefes.containsKey(indexSavingKey)) {
+          //this will return last index of wordlist that user answered correct
+          index = prefes.getInt(indexSavingKey);
+          //just goto next word because saved last index was user answered correctly
+          index++;
+        }
+      }
+
       preposition = wordList[index];
       totalScroe = wordList.length;
 
       if (isGeneratedNewList) {
         charList = getWordsLetterList(wordList[index].word);
         correctWord = preposition.word;
-        fillInTheBlankHint =
-            preposition.fillInTheGapSentence.replaceAll(
-                correctWord, '________');
+        fillInTheBlankHint = preposition.fillInTheGapSentence
+            .replaceAll(correctWord, '________');
         isGeneratedNewList = false;
       }
       meaning = preposition.meaning;
     }
 
-    return isLoaded ? Scaffold(
-      backgroundColor: Colors.white,
-      bottomNavigationBar: BottomAppBar(
-        color: HexColor('${category.color}'),
-        child: new Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: IconButton(
-                icon: Icon(
-                  Icons.arrow_back_ios,
-                  color: HexColor('#444444'),
+    return isLoaded
+        ? Scaffold(
+            backgroundColor: Colors.white,
+            bottomNavigationBar: BottomAppBar(
+              color: HexColor('${category.color}'),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: HexColor('#444444'),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          //do not go -1 index of word list cause that's impossible.
+                          //there is no word
+                          if (index != 0){
+                            gotoPreviousWord();
+                            setState() {
+                              index--;
+                              isGeneratedNewList = true;
+                            }
+                          } else {
+                            //show no previous word dialog
+                            showWrongAnswerDialog('You can not go previous');
+                          }
+
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: IconButton(
+                      icon: Icon(Icons.autorenew, color: HexColor('#444444')),
+                      onPressed: () {
+                        setState(() {
+                          guessedWord = '';
+                          charList = getWordsLetterList(wordList[index].word);
+                          setState() {
+                            isGeneratedNewList = true;
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_forward_ios,
+                          color: HexColor('#444444')),
+                      onPressed: () {
+                        setState(() {
+                          var nextWordId = wordList[index].id.toString();
+                          if (prefes.containsKey(nextWordId)) {
+                            gotoNextWord();
+
+                            setState() {
+                              index++;
+                              isGeneratedNewList = true;
+                            }
+
+                          } else {
+                            showWrongAnswerDialog('can not go to next!');
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            body: Column(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 80.0),
+                  child: Text(
+                    '$index/$totalScroe',
+                    style: TextStyle(
+                        fontSize: 60.0,
+                        fontWeight: FontWeight.bold,
+                        color: HexColor('#D5D5D5')),
+                  ),
                 ),
-                onPressed: () {
-                  setState(() {
-                    gotoPreviousWord();
-                    userScore--;
-                    setState() {
-                      isGeneratedNewList = true;
-                    }
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.autorenew, color: HexColor('#444444')),
-                onPressed: () {
-                  setState(() {
-                    guessedWord = '';
-                    charList = getWordsLetterList(wordList[index].word);
-                    setState() {
-                      isGeneratedNewList = true;
-                    }
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.arrow_forward_ios, color: HexColor('#444444')),
-                onPressed: () {
-                  setState(() {
-                    homeText = 'forward clicked';
-                    debugPrint(homeText);
-
-                    var nextWordId = wordList[index].id.toString();
-
-                    print('next word id: ${wordList[index].id}');
-                    print('is next word saved: ${prefes.containsKey(
-                        nextWordId)}');
-                    if (prefes.containsKey(nextWordId)) {
-                      gotoNextWord();
-
-                      userScore++;
-                      setState() {
-                        isGeneratedNewList = true;
-                      }
-                    } else {
-                      showWrongAnswerDialog('can not go to next!');
-                      print('cant go to next');
-                    }
-                  });
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(top: 80.0),
-            child: Text(
-              '$userScore/$totalScroe',
-              style: TextStyle(
-                  fontSize: 60.0,
-                  fontWeight: FontWeight.bold,
-                  color: HexColor('#D5D5D5')),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(top: 100.0),
-            child: guessedWord == ''
-                ? Text(
-              '$placeholder_guessed_word ',
-              style: TextStyle(
-                  color: HexColor('#D5D5D5'),
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w500),
-            )
-                : Text(
-              '$guessedWord',
-              style:
-              TextStyle(fontSize: 50.0, fontWeight: FontWeight.w500),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 50.0),
-            width: MediaQuery
-                .of(context)
-                .size
-                .width - 40.0,
-            child: Text(
-              'Fill in the gap:\n $fillInTheBlankHint;',
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Container(
-            height: 55.0,
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(top: 50.0),
-            child: new ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: charList.length,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    child: GestureDetector(
-                        behavior: HitTestBehavior.translucent,
-                        onTap: () {
-                          var tappedLetter = '$guessedWord${charList[index]}';
-                          setState(() {
-                            guessedWord = tappedLetter;
-                          });
-                          debugPrint('clicked');
-                          checkAnswer(meaning);
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 100.0),
+                  child: guessedWord == ''
+                      ? Text(
+                          '$placeholder_guessed_word ',
+                          style: TextStyle(
+                              color: HexColor('#D5D5D5'),
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.w500),
+                        )
+                      : Text(
+                          '$guessedWord',
+                          style: TextStyle(
+                              fontSize: 50.0, fontWeight: FontWeight.w500),
+                        ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 50.0),
+                  width: MediaQuery.of(context).size.width - 40.0,
+                  child: Text(
+                    'Fill in the gap:\n $fillInTheBlankHint;',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  height: 55.0,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 50.0),
+                  child: new ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: charList.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, i) {
+                        return GestureDetector(
+                          child: GestureDetector(
+                              behavior: HitTestBehavior.translucent,
+                              onTap: () {
+                                var tappedLetter =
+                                    '$guessedWord${charList[i]}';
+                                setState(() {
+                                  guessedWord = tappedLetter;
+                                });
+                                debugPrint('clicked');
+                                checkAnswer(meaning);
 //                        debugPrint(charList[index]);
-                        },
-                        child: Container(
-                          alignment: Alignment.center,
-                          width: 55.0,
-                          margin: EdgeInsets.all(2.0),
-                          color: HexColor('${category.color}'),
-                          child: Text(
-                            charList[index],
-                            style: TextStyle(
-                                fontSize: 16.0, fontWeight: FontWeight.bold),
-                          ),
-                        )),
-                  );
-                }),
+                              },
+                              child: Container(
+                                alignment: Alignment.center,
+                                width: 55.0,
+                                margin: EdgeInsets.all(2.0),
+                                color: HexColor('${category.color}'),
+                                child: Text(
+                                  charList[i],
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              )),
+                        );
+                      }),
+                )
+              ],
+            ),
           )
-        ],
-      ),
-    ) : Center(
-      child: CircularProgressIndicator(),
-    );
+        : Center(
+            child: CircularProgressIndicator(),
+          );
   }
 
   void gotoNextWord() {
@@ -400,6 +349,10 @@ class _GamePlayPageState extends State<GamePlayPage> {
       debugPrint("Answer is correct");
       print('correct ans word id: ${wordList[index].id.toString()}');
       prefes.setString(wordList[index].id.toString(), '');
+      //set last index that user answered correctly
+      var categoryName = category.name.toLowerCase();
+      var indexSavingKey = categoryName + "index";
+      prefes.setInt(indexSavingKey, index);
       showCorrectAnswerDialog(meaning);
     } else {
       debugPrint("Wrong answer");
@@ -437,5 +390,94 @@ class _GamePlayPageState extends State<GamePlayPage> {
       });
     });
   }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    List<Word> words = await WordyPreference.fetchListAsString(category.name);
+//
+//    print('fetcing from preferences: length - ${words.length}');
+//
+//    for(int i=0; i<words.length; i++){
+//      print('\n');
+//      print(words[i].word);
+////      print(word.id.toString());
+//    }
+//    print('fetching done!');
+//
+//  }
+
+//  void setList() async {
+//    List<Word> l = await WordyPreference.fetchListAsString(category.name);
+//    list = l;
+//  }
+
+//    String wordListInString = json.encode(l);
+//    List<Word> list = json.decode(myData);
+////
+//    for (var word in myData) {
+//      debugPrint('\n');
+//      debugPrint('${word['id']}');
+//      debugPrint('${word['word']}');
+//      debugPrint('${word['meaning']}');
+//      debugPrint('${word['fillInTheGapSentence']}');
+//    }
+
+
+
+
+
+//    setCategoryList(category.name);
+//
+//    preposition = wordList[index];
+//    totalScroe = wordList.length;
+
+//    setList();
+//    testJson();
+//    fetchListAsString(category.name);
+
+
+
+//  fetchListAsString(String categoryName) async {
+//    final prefs = await SharedPreferences.getInstance();
+//
+//    if (prefs.containsKey('preposition')) {
+//      debugPrint('Contains key');
+//
+//      var data = prefs.get('preposition') as String;
+//      var myData = json.decode(data);
+//
+//      for (var word in myData) {
+//
+//        var stringId = '${word['id']}';
+//        int id = int.parse(stringId);
+//        wordList.add(new Word(id, '${word['word']}', '${word['meaning']}',
+//            '${word['fillInTheGapSentence']}'));
+////       debugPrint('${word['id']}');
+////      debugPrint('${word['word']}');
+////      debugPrint('${word['meaning']}');
+////      debugPrint('${word['fillInTheGapSentence']}');
+//      }
+//    } else {
+//      debugPrint('Not contains key');
+//    }
+//
+//
+//    for (var l in wordList) {
+//      debugPrint(l.word);
+//    }
+//  }
